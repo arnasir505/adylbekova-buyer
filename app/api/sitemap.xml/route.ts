b62eb485bf58f44975b526bdef25dbab7bedec7c
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2025-03-31 15:47:05 +0600

    Fix sitemap url

---
+++ app/api/sitemap.xml/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest } from 'next/server';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function GET(req: NextRequest) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://adylbekovabuyer.com/</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
